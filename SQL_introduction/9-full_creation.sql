483396f3a852aff999d7dae9968ce042ccd254c3
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-08-15 04:05:33 +0300

    finalize

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,17 @@
---script that creates a table `second_table` in the database hbtn_0c_0
-CREATE TABLE IF NOT EXISTS second_table(
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR (256),
-    score INT UNSIGNED
+--Create second tale and insert values
+USE hbtn_0c_0;
+-- Create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT PRIMARY KEY,
+    name VARCHAR(256),
+    score INT
 );
-INSERT INTO `second_table` (`id`, `name`, `score`)
-VALUES(1, 'John', '10'),
-    (2, 'Alex', 3),
-    (3, 'Bob', 14),
-    (4, 'George', 8);
+-- Insert rows into the table
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score)
+VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score)
+VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score)
+VALUES (4, 'George', 8);

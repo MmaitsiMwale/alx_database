dd8d1e900721aa604761d8f7b28c8c2044eb9537
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-08-15 03:56:07 +0300

    finalize

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+--script that creates a table `second_table` in the database hbtn_0c_0
+CREATE TABLE IF NOT EXISTS second_table(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (256),
+    score INT UNSIGNED
+);
+INSERT INTO `second_table` (`id`, `name`, `score`)
+VALUES(1, 'John', '10'),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);

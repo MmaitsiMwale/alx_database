55050283d3203faecd5619516ae49a172b8e4dbc
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-08-15 04:31:08 +0300

    finalize

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,9 @@
 --Create second tale and insert values
 -- USE hbtn_0c_0 IF EXISTS;
 -- Create the table if it doesn't exist
-CREATE TABLE IF NOT EXISTS second_table (
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
     id INT PRIMARY KEY,
-    name VARCHAR(256),
+    `name` VARCHAR(256),
     score INT
 );
 -- Insert rows into the table

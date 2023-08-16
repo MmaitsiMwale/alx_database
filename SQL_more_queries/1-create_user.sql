44e9fcc48976333aa8937d00e2c60f07293b750a
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-08-16 03:14:11 +0300

    readme and start

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- creates the user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1' @'localhost' IDENTIFIED WITH auth_socket BY 'user_0d_1_pwd'
+CREATE USER IF NOT EXISTS 'user_0d_1' @'localhost' IDENTIFIED WITH auth_socket BY 'user_0d_1_pwd';
